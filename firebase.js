dea08151a0608275edd0c7d7084ac1ad73857788
Giovanni Rodrigues <giovannicorrearodrigues124@gmail.com>
2026-04-26 10:42:17 -0300

    Remove sensitive data from firebase configuration
    Updated firebase configuration to remove sensitive information.

--- firebase.js
+++ firebase.js
@@ -6,14 +6,12 @@ import { getFirestore }
   from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey:        "AIzaSyAO7EWk7wx1dgX7aFNQzZGuq04U5CgxSW4",
-  authDomain:    "clinica-661a4.firebaseapp.com",
-  projectId:     "clinica-661a4",
-  storageBucket: "clinica-661a4.appspot.com"
-  // messagingSenderId e appId são opcionais para Auth + Firestore básico
+  apiKey:"",
+  authDomain:"",
+  projectId:"",
+  storageBucket""
 };
 
-// Evita dupla inicialização caso o módulo seja importado mais de uma vez
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
 export const auth = getAuth(app);
